e4432e1825d73622955bdb38cbcc27eb4837b3ef
greedyjude <jude.osbert@greedygame.com>
2020-10-16 11:16:44 +0530

    Update to v0.0.83

--- app/src/main/java/com/greedygame/sdkxinterstitialdemo/SecondActivity.java
+++ app/src/main/java/com/greedygame/sdkxinterstitialdemo/SecondActivity.java
@@ -1,59 +1,38 @@
 package com.greedygame.sdkxinterstitialdemo;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ProgressBar;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import com.greedygame.core.AppConfig;
-import com.greedygame.core.GreedyGameAds;
 import com.greedygame.core.adview.modals.AdRequestErrors;
 import com.greedygame.core.interstitial.general.GGInterstitialAd;
 import com.greedygame.core.interstitial.general.GGInterstitialEventsListener;
 
-import java.util.logging.Logger;
-
-
-public class MainActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity {
     GGInterstitialAd ggInterstitialAd;
-
     private boolean shouldShowAd = true;
-    /*
-    Control the display of ads using shouldShowAd flag
-     */
-
-    private InterstitialEventListener eventListener = new InterstitialEventListener();
     private Button loadAdAgainButton;
-    private ProgressBar progressBar;
+    private InterstitialEventListener eventListener = new InterstitialEventListener();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        /*
-        Initializing the SDK
-         */
-        AppConfig appConfig = new AppConfig.Builder(this)
-                .withAppId("89221032")
-                .build();
-        GreedyGameAds.initWith(appConfig,null);
+        setContentView(R.layout.activity_second);
         ggInterstitialAd  = new GGInterstitialAd(this,"float-4839");
-
-        progressBar = findViewById(R.id.progressBar);
-        loadAdAgainButton = findViewById(R.id.loadAdAgain);
-        loadAdAgainButton.setOnClickListener(new View.OnClickListener(){
+        loadAdAgainButton = findViewById(R.id.showAgainButton);
+        loadAdAgainButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 showAdAgain();
             }
         });
-
         loadIntersitialAd();
     }
+
 
     private void showAdAgain(){
         if(ggInterstitialAd.isAdLoaded()){
@@ -62,7 +41,6 @@ public class MainActivity extends AppCompatActivity {
              loaded, we show it here.
              */
             ggInterstitialAd.show();
-
         }
         else{
             /*
@@ -76,10 +54,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void loadIntersitialAd(){
-        progressBar.setVisibility(View.VISIBLE);
         ggInterstitialAd.loadAd(eventListener);
     }
-    class InterstitialEventListener implements GGInterstitialEventsListener{
+
+
+
+    class InterstitialEventListener implements GGInterstitialEventsListener {
         @Override
         public void onAdLeftApplication() {
 
@@ -88,9 +68,9 @@ public class MainActivity extends AppCompatActivity {
         public void onAdClosed() {
             // Setting flag to false to not show the ad again. This covers the case of opening
             // and ad that is already loaded
-            Log.d("GGAD","Ad Closed - 1");
+            Log.d("GGAD","Ad Closed - 2");
             shouldShowAd = false;
-            startActivity(new Intent(MainActivity.this,SecondActivity.class));
+            startActivity(new Intent(SecondActivity.this,MainActivity.class));
             finish();
         }
         @Override
@@ -100,14 +80,12 @@ public class MainActivity extends AppCompatActivity {
         }
         @Override
         public void onAdLoadFailed(AdRequestErrors cause) {
-            progressBar.setVisibility(View.GONE);
-            Toast.makeText(MainActivity.this, "Ad Load failed "+cause.toString(), Toast.LENGTH_SHORT).show();
+            Toast.makeText(SecondActivity.this, "Ad Load failed "+cause.toString(), Toast.LENGTH_SHORT).show();
             //Called when the ad load failed. The reason is available in cause variable
 
         }
         @Override
         public void onAdLoaded() {
-            progressBar.setVisibility(View.GONE);
             if(shouldShowAd){
                 // Setting flag to false to not show the ad again.
                 shouldShowAd = false;
@@ -116,7 +94,4 @@ public class MainActivity extends AppCompatActivity {
 
         }
     }
-
-
 }
-
